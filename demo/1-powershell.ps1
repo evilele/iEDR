a5dc1023e9a45c8ed2570981f4ebbe36d882eb47
cailllev <cailllev@students.zhaw.ch>
2026-06-02 20:35:36 +0200

    do not copy file, just start it

--- demo/1-powershell.ps1
+++ demo/1-powershell.ps1
@@ -1,16 +1,7 @@
 $r = $PSScriptRoot
 $s = "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe"
-$p = "C:\Users\Public\Downloads\powershell.exe"
 
-if (Test-Path $p) {
-	rm $p -Force
-}
+Start-Process "$r\..\iEDR.exe" -Args "-a $s"
 
-Start-Process "$r\..\iEDR.exe" -Args "-a $p"
-
-$_= Read-Host "Wait for iEDR startup..."
-
-cp $s $p
-Write-Host "Copied $s to $p"
-Read-Host "Press ENTER to start $p"
-Start-Process $p
+$_= Read-Host "Wait for iEDR startup, then press ENTER to start $s"
+Start-Process $s
